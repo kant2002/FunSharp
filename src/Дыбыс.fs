fe7aae21c0a1edf11dbddce4c85c4cd1c9c3908f
Andrii Kurdiumov <kant2002@gmail.com>
2023-01-29 15:25:20 +0600

    Қазақ тіліне аудару

---
+++ src/Дыбыс.fs
@@ -0,0 +1,20 @@
+﻿module Library.Дыбыс
+
+open System.Media
+
+type private IMarker = interface end
+
+let private тарту есім =
+    let қүрастыру = System.Reflection.Assembly.GetAssembly(typeof<IMarker>)
+    let ағын = қүрастыру.GetManifestResourceStream(есім+".wav")
+    let ойнатқыш = new SoundPlayer(ағын)
+    ойнатқыш.Play()   
+
+let ТартуҚоңырауды () =
+    тарту "BellRing"
+
+let ТартуЦимбалдар () =
+    тарту "Chime"
+
+let ТартуБасыңыз () =
+    тарту "Click"
